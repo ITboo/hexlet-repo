6e480b53fe72396b1219705e6c62e35067745b73
ITboo <annaacdc@tut.by>
2024-07-19 18:56:30 +0300

    add 1-3 php lessons

---
+++ php/2/instructions.php
@@ -0,0 +1,10 @@
+/* 
+* Выведите на экран друг за другом три имени: Robert, Stannis, Renly. 
+* Для каждого имени используйте свой вызов print_r().
+*/
+
+<?php
+
+print_r('Robert');
+print_r('Stannis');
+print_r('Renly');
